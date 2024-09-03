ea2d27cd21f3647da479b096897c8c0f00f5dc2c
Clint Harvey Gamolo <csheesh34@protonmail.com>
2024-09-03 12:06:57 +0800

    Feat: Added new packages and changed website theme

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -6,7 +6,7 @@ import '../styles/Body.css'
 const userDescription: JSX.Element = (
     <div>
         <p>
-            I'm a Computer Science Student @<a href="https://www.xu.edu.ph/">Xavier University Ateneo de Cagayan</a>. I love technology, my areas of interest currently are in web development, game development, and cybersecurity. 
+            I'm a Computer Science Student @<a href="https://www.xu.edu.ph/">Xavier University Ateneo de Cagayan</a>. I love technology, my areas of interest currently are in <strong>web development</strong>, <strong>game development</strong>, and <strong>cybersecurity</strong>. 
         </p>
         <p>
             This website is going to be the hub for everything about me. 
@@ -23,6 +23,7 @@ function Body() {
         <>
             <div className="body">
                 <Welcome userName='Clint' imagePath={UserImage} userDescription={userDescription}/>
+                <hr />
                 <Posts />
             </div>
         </>
