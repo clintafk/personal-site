d0b0a0642d9f25a9cd702216a7648a8ef757a0f4
Clint Harvey Gamolo <csheesh34@protonmail.com>
2024-09-03 12:08:03 +0800

    Feat: Added icons to navbar links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,19 @@ import logo from '../../public/logo.svg';
 import resume from '../../public/resume.pdf';
 import { NavLink } from 'react-router-dom'
 
+import { FaLinkedin, FaFacebookSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FcHome } from "react-icons/fc";
+import { FcAddressBook } from "react-icons/fc";
+import { FcFolder } from "react-icons/fc";
+import { FcCellPhone } from "react-icons/fc";
+
 function Header() {
     return(
         <header className='header'>
             <div className="header-logo">
                 <a href="">
-                    <img src={logo} alt="website logo" />
+                    Clint Harvey
                 </a>
             </div>
             <div className='header-nav'>
@@ -23,6 +30,9 @@ function Header() {
                                         isPending ? "pending" : isActive ? "active" : ""
                                     }
                                 >
+                                    <div className="nav-icon">
+                                        <FcHome />
+                                    </div>
                                     Home
                                 </NavLink>
                             </a>
@@ -35,18 +45,49 @@ function Header() {
                                         isPending ? "pending" : isActive? "active" : ""
                                     }
                                 >
+                                    <div className="nav-icon">
+                                        <FcAddressBook />
+                                    </div>
                                     Blog
                                 </NavLink>
                             </a>
                         </li>
                         <li>
                             {/* <a href={resume} download="clint_resume">CV</a> */}
-                            <a href={resume} target='_blank'>CV</a>
+                            <a href={resume} target='_blank'>
+                                <div className="nav-icon">
+                                    <FcFolder />
+                                </div>
+                                Portfolio
+                            </a>
                         </li>
                         <li>
-                            <a href="">Contact</a>
+                            <a href="">
+                                <div className="nav-icon">
+                                    <FcCellPhone />
+                                </div>
+                                Contact
+                            </a>
                         </li>
                     </ul>
+                    {/* <ul className='icon-list'>
+                        <li>
+                            <a href="">
+                                <FaLinkedin />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <FaSquareXTwitter />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <FaFacebookSquare />
+                            </a>
+                        </li>
+
+                    </ul> */}
                 </nav>
             </div>
         </header>
